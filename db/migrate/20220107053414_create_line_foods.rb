7c13172c600d9ef25037a25ad4655d167f4e084c
suzuki-motomichi <cw.sm31@gmail.com>
2022-01-07 17:18:48 +0900

    Add: テーブル作成

---
+++ db/migrate/20220107053414_create_line_foods.rb
@@ -0,0 +1,13 @@
+class CreateLineFoods < ActiveRecord::Migration[6.1]
+  def change
+    create_table :line_foods do |t|
+      t.references :food, null: false, foregin_key: true
+      t.references :restaurant, null: false, foregin_key: true
+      t.references :order, foregin_key: true
+      t.integer :count, null: false, default: 0
+      t.boolean :active, null: false, default: false
+
+      t.timestamps
+    end
+  end
+end
